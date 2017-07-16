79783c4d1becd4473af88a88e8d9adc7b18320ea
dylanfisher <hi@dylanfisher.com>
2017-07-16 02:02:50 -0400

    Home page javascript and montauk page svg icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log('main.js -> hello world');
+// Namespace
+var App = {};
+
+// Load
+$(function() {
+  App.scrollTop = $(window).scrollTop();
+  App.windowWidth  = $(window).width();
+  App.windowHeight = $(window).height();
+});
+
+// Resize
+$(window).on('resize', function() {
+  App.windowWidth = $(window).width();
+  App.windowHeight = $(window).height();
+});
+
+// Scroll
+$(window).on('scroll', function() {
+  App.scrollTop = $(window).scrollTop();
+});
+
+// Custom scripts
+$(function() {
+  var $html = $('html');
+  var $homeContent = $('.home-content');
+  var $nav = $('.nav');
+
+  if ( $homeContent.length ) {
+    var homeContentoffset = $homeContent.offset().top;
+    var navHeight = $nav.height() + $nav.position().top;
+    var offset = homeContentoffset - navHeight;
+
+    $(window).on('scroll.homeContentNav', function() {
+      if ( App.scrollTop >= offset ) {
+        $html.addClass('nav-active');
+      } else {
+        $html.removeClass('nav-active');
+      }
+    });
+
+    $(window).trigger('scroll.homeContentNav');
+  } else {
+    $(window).off('scroll.homeContentNav');
+  }
+});
