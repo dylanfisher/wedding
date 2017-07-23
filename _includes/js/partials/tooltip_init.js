f6bd40c96c05d48f996a7c17b5cb72bd6a9bf02f
dylanfisher <hi@dylanfisher.com>
2017-07-23 18:16:30 -0400

    jQuery UI tooltips for image captions

---
+++ _includes/js/partials/tooltip_init.js
@@ -0,0 +1,13 @@
+// Tooltips
+
+$(function() {
+  $(document).tooltip({
+    track: true,
+    items: 'img',
+    position: {
+      my: 'left top+15',
+      at: 'left bottom',
+      collision: 'flipfit'
+    }
+  });
+});
